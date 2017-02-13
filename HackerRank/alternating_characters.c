7aca62934d42de4b28e0b957e11e88b7e6f4ec4f
NikhitaGaonkar <NikhitaGaonkar@users.noreply.github.com>
2017-02-13 11:03:08 -0500

    Add files via upload

--- HackerRank/alternating_characters.c
+++ HackerRank/alternating_characters.c
@@ -3,6 +3,20 @@
 #include <math.h>
 #include <stdlib.h>
 
+/*
+  Shashank likes strings in which consecutive characters are different. For example, he likes ABABA, while he doesn't like ABAA.
+  Given a string containing characters A and B only, he wants to change it into a string he likes. 
+  To do this, he is allowed to delete the characters in the string.
+  
+  Your task is to find the minimum number of required deletions.
+  
+  AAAA  A, 3 deletions
+  BBBBB  B, 4 deletions
+  ABABABAB  ABABABAB, 0 deletions
+  BABABA  BABABA, 0 deletions
+  AAABBB  AB, 4 deletions because to convert it to AB we need to delete 2 A's and 2 B's
+*/
+
 int alternating_char(char str[])
 {
    int n = strlen(str);
