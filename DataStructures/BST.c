34cd0d9b9e78db0c808e56d45cc8ab33c6c5fb89
NikhitaGaonkar <NikhitaGaonkar@users.noreply.github.com>
2017-03-19 20:03:17 -0400

    Add files via upload

--- DataStructures/BST.c
+++ DataStructures/BST.c
@@ -1,107 +1,77 @@
 #include<stdio.h>
 #include<stdlib.h>
 
+/*
+   Binary Search Trees
+*/
 typedef struct treeNode
 {
         int data;
+		int count; //no of nodes at current node
         struct treeNode *left;
         struct treeNode *right;
 
 }treeNode;
 
-treeNode* FindMin(treeNode *node)
+/*
+  Insert and Find
+  Number of compares for search and insert = 1+depth of node.
+  The shape of the tree depends on order of insertion, 
+  if the nodes come in order then it is basically no different than a linked list worst height is N
+  
+  There is 1:1 one correspondence between BST and  quick sort, if there are no duplicates.
+  
+  Complexity: O(logN)
+*/
+treeNode * insert(treeNode *node, int data)
 {
         if(node==NULL)
         {
-                /* There is no element in the tree */
-                return NULL;
-        }
-        if(node->left) /* Go to the left sub tree to find the min element */
-                return FindMin(node->left);
-        else 
+               
+                node = (treeNode *)malloc(sizeof(treeNode));
+                node-> data = data;
+                node-> left = node-> right = NULL;
                 return node;
-}
-treeNode* FindMax(treeNode *node)
-{
-        if(node==NULL)
-        {
-                /* There is no element in the tree */
-                return NULL;
-        }
-        if(node->right) /* Go to the left sub tree to find the min element */
-                return FindMax(node->right);
-        else 
-                return node;
-}
-
-treeNode * Insert(treeNode *node,int data)
-{
-        if(node==NULL)
-        {
-                treeNode *temp;
-                temp = (treeNode *)malloc(sizeof(treeNode));
-                temp -> data = data;
-                temp -> left = temp -> right = NULL;
-                return temp;
         }
 
         if(data >(node->data))
         {
-                node->right = Insert(node->right,data);
+                node->right = insert(node->right,data);
         }
         else if(data < (node->data))
         {
-                node->left = Insert(node->left,data);
+                node->left = insert(node->left,data);
         }
+		
+		node->count = 1 + get_size(node->left) + get_size(node->right);
+		
         /* Else there is nothing to do as the data is already in the tree. */
         return node;
 
 }
 
-treeNode * Delete(treeNode *node, int data)
+int get_size(treeNode *node)
 {
-        treeNode *temp;
-        if(node==NULL)
-        {
-                printf("Element Not Found");
-        }
-        else if(data < node->data)
-        {
-                node->left = Delete(node->left, data);
-        }
-        else if(data > node->data)
-        {
-                node->right = Delete(node->right, data);
-        }
-        else
-        {
-                /* Now We can delete this node and replace with either minimum element 
-                   in the right sub tree or maximum element in the left subtree */
-                if(node->right && node->left)
-                {
-                        /* Here we will replace with minimum element in the right sub tree */
-                        temp = FindMin(node->right);
-                        node -> data = temp->data; 
-                        /* As we replaced it with some other node, we have to delete that node */
-                        node -> right = Delete(node->right,temp->data);
-                }
-                else
-                {
-                        /* If there is only one or zero children then we can directly 
-                           remove it from the tree and connect its parent to its child */
-                        temp = node;
-                        if(node->left == NULL)
-                                node = node->right;
-                        else if(node->right == NULL)
-                                node = node->left;
-                        free(temp); /* temp is longer required */ 
-                }
-        }
-        return node;
-
+	if(node == NULL) return 0;
+    return node->count;
 }
 
-treeNode * Find(treeNode *node, int data)
+//find element iterative and recursive
+treeNode *find_iterative(treeNode *node, int data)
+{
+	treeNode *root = node;
+	while(root != NULL)
+	{
+		if(root->data < data)
+			root = root->right;
+		else if(root->data > data)
+			root = root->left;
+		else return root;
+	}
+    return NULL;
+}
+
+treeNode * find_recursive(treeNode *node, int data)
 {
         if(node==NULL)
         {
@@ -111,12 +81,12 @@ treeNode * Find(treeNode *node, int data)
         if(data > node->data)
         {
                 /* Search in the right sub tree. */
-                return Find(node->right,data);
+                return find_recursive(node->right,data);
         }
         else if(data < node->data)
         {
                 /* Search in the left sub tree. */
-                return Find(node->left,data);
+                return find_recursive(node->left,data);
         }
         else
         {
@@ -125,62 +95,71 @@ treeNode * Find(treeNode *node, int data)
         }
 }
 
-void PrintInorder(treeNode *node)
+/*
+   Ordered Operations
+*/
+
+treeNode* find_min(treeNode *node)
 {
         if(node==NULL)
         {
-                return;
+                /* There is no element in the tree */
+                return NULL;
         }
-        PrintInorder(node->left);
-        printf("%d ",node->data);
-        PrintInorder(node->right);
+        if(node->left) /* Go to the left sub tree to find the min element */
+                return find_min(node->left);
+        else 
+                return node;
 }
-
-void PrintPreorder(treeNode *node)
+treeNode* find_max(treeNode *node)
 {
         if(node==NULL)
         {
-                return;
+                /* There is no element in the tree */
+                return NULL;
         }
-        printf("%d ",node->data);
-        PrintPreorder(node->left);
-        PrintPreorder(node->right);
-}
-
-void PrintPostorder(treeNode *node)
-{
-        if(node==NULL)
-        {
-                return;
-        }
-        PrintPostorder(node->left);
-        PrintPostorder(node->right);
-        printf("%d ",node->data);
+        if(node->right) /* Go to the left sub tree to find the min element */
+                return find_max(node->right);
+        else 
+                return node;
 }
 
 
-int MaxDepth(treeNode *root)
+//depth = no of nodes
+int max_depth(treeNode *node)
 {
-	if(root == NULL)
+	if(node == NULL)
 		return 0;
-	int ldepth = MaxDepth(root->left);
-	int rdepth = MaxDepth(root->right);
+	int ldepth = max_depth(node->left);
+	int rdepth = max_depth(node->right);
 	
 	if(ldepth < rdepth)
-    {		
-		return rdepth+1;
-	}
+   		return rdepth+1;
+	else
+	  return ldepth+1;
+}
+
+//depth = no of edges
+int max_depth_edges(treeNode *node)
+{
+	if(node == NULL ||(node->left == NULL && node->right == NULL))
+		return 0;
 	else
 	{
-	    return ldepth+1;
+		int ldepth = max_depth_edges(node->left);
+		int rdepth = max_depth_edges(node->right);
+		
+		return (ldepth > rdepth ? ldepth+1 : rdepth+1);
 	}
+	
 }
+
 
 void print_path(int *path, int height)
 {
 	int i =0;
 	for(i=0; i<height; i++)
-		printf("%d", path[i]);
+		printf("%d ", path[i]);
 	printf("\n");
 }
 
@@ -203,50 +182,237 @@ void print_longest_path(treeNode *root, int *path, int height, int iter)
 
 }
 
+
+/*
+ *  Get the number of nodes < key/value.
+ *            S  (8) ->size
+ *           / \
+ *          /   \
+ *         /     \
+ *     (4)E       W(3)
+ *       / \      /\
+ *      /   \    /  \
+ *  (2)A     H  T    Z (1)
+ *      \
+ *       C
+ *
+ *  For E -> rank is get_size(A) = 2
+ */
+ int get_rank(treeNode *node, int data)
+ {
+	if(node == NULL) return 0;
+	
+	if(data < node->data)
+		return get_rank(node->left, data);
+	else if(data > node->data)
+		//root node + no of node less than root+ rank on the right subtree
+		return 1 + get_size(node->left) + get_rank(node->right, data);
+	else
+		//data = node->data
+		return get_size(node->left);
+	 
+ }
+ 
+ 
+ /*
+     Top View
+     		 3
+		   /   \
+		  5     2
+		 / \   / \
+		1   4 6   7
+		 \       /
+ 		  9     8
+    top view: 1->5->3->2->7
+ */
+void print_left_nodes(treeNode *node)
+{
+	if(node == NULL) return;
+	print_left_nodes(node->left);
+	printf("%d ", node->data);
+}
+void top_view(treeNode *root)
+{
+	treeNode *temp = root;
+	if(root == NULL) return;
+	
+	print_left_nodes(root->left);
+	printf("%d ", root->data);
+	//print all right nodes
+	while(temp != NULL)
+	{
+		temp = temp->right;
+		printf("%d ", temp->data);
+	}
+}
+void top_view2(treeNode *root)
+{
+	static treeNode *curr_root = root;
+	if(root == NULL) return ;
+	//recur on the left
+	top_view2(root->left);
+	printf("%d ", root->data);
+	//for left node curr_root is always previous left node
+	//for root it is root
+	if(curr_root == root)
+	{
+		printf("%d ", root->data);
+		root = root->right;
+		while(root)
+		{
+			printf("%d ", root->data);
+			root = root->right;
+		}
+	}
+}
+
+ 
+/*
+   Delete node in BST.
+*/
+treeNode * delete_node(treeNode *node, int data)
+{
+        treeNode *temp;
+        if(node==NULL)
+        {
+                printf("Element Not Found");
+        }
+        else if(data < node->data)
+        {
+                node->left = delete_node(node->left, data);
+        }
+        else if(data > node->data)
+        {
+                node->right = delete_node(node->right, data);
+        }
+        else
+        {
+                /* Now We can delete this node and replace with either minimum element 
+                   in the right sub tree or maximum element in the left subtree */
+                if(node->right && node->left)
+                {
+                        /* Here we will replace with minimum element in the right sub tree */
+                        temp = find_min(node->right);
+                        node -> data = temp->data; 
+                        /* As we replaced it with some other node, we have to delete that node */
+                        node -> right = delete_node(node->right,temp->data);
+                }
+                else
+                {
+                        /* If there is only one or zero children then we can directly 
+                           remove it from the tree and connect its parent to its child */
+                        temp = node;
+                        if(node->left == NULL)
+                                node = node->right;
+                        else if(node->right == NULL)
+                                node = node->left;
+                        free(temp); /* temp is longer required */ 
+                }
+        }
+        return node;
+
+}
+
+/*
+ *    Tree Traversal
+ 
+ *           7
+ *          / \
+ *         /   \
+ *        4     8    
+ *       / \
+ *      /   \
+ *     1     6
+ *
+ *   Depth first search
+ */
+
+void print_inorder(treeNode *node)
+{
+        if(node==NULL)
+        {
+                return;
+        }
+        print_inorder(node->left);
+        printf("%d ",node->data);
+        print_inorder(node->right);
+}
+
+void print_preorder(treeNode *node)
+{
+        if(node==NULL)
+        {
+                return;
+        }
+        printf("%d ",node->data);
+        print_preorder(node->left);
+        print_preorder(node->right);
+}
+
+void print_postorder(treeNode *node)
+{
+        if(node==NULL)
+        {
+                return;
+        }
+        print_postorder(node->left);
+        print_postorder(node->right);
+        printf("%d ",node->data);
+}
+
+
+
+//////////////////////Driver Program///////////////////////
 int main()
 {
         treeNode *root = NULL;
-        root = Insert(root, 5);
-        root = Insert(root, -1);
-        root = Insert(root, 3);
-        root = Insert(root, -14);
-        root = Insert(root, 8);
-        root = Insert(root, 10);
-        root = Insert(root, 9);
-        root = Insert(root, 6);
-        PrintInorder(root);
+        root = insert(root, 5);
+        root = insert(root, -1);
+        root = insert(root, 3);
+        root = insert(root, -14);
+        root = insert(root, 8);
+        root = insert(root, 10);
+        root = insert(root, 9);
+        root = insert(root, 6);
+		
+        print_inorder(root);
         printf("\n");
-        //root = Delete(root,5);
-        //root = Delete(root,-1);
-        PrintInorder(root);
+        print_preorder(root);
         printf("\n");
-        treeNode * temp;
-        temp = FindMin(root);
-        printf("Minimum element is %d\n",temp->data);
-        temp = FindMax(root);
-        printf("Maximum element is %d\n",temp->data);
-        temp = Find(root,8);
-        if(temp==NULL)
-        {
-                printf("Element 8 not found\n");
-        }
-        else
-        {
-                printf("Element 8 Found\n");
-        }
-        temp = Find(root,2);
-        if(temp==NULL)
-        {
-                printf("Element 2 not found\n");
-        }
-        else
-        {
-                printf("Element 6 Found\n");
-        }
-
-        int height = MaxDepth(root);
-        printf("%d \n", height);
         
+        	
+        int height = max_depth(root);
+        printf("Max height : %d \n", height);
+       	printf("Max height edges: %d \n", max_depth_edges(root));
+		       
+        int rank_root = get_rank(root, 5);
+		printf("No of elements less than root %d \n", get_size(root->left));
+        
+        root = delete_node(root,5);
+        root = delete_node(root,-1);
+        
+		print_inorder(root);
+        printf("\n");
+		
+        treeNode * temp;
+        temp = find_min(root);
+        printf("Minimum element is %d\n",temp->data);
+        temp = find_max(root);
+        printf("Maximum element is %d\n",temp->data);
+
+        temp = find_iterative(root,8);
+        if(temp==NULL)
+          printf("Element 8 not found\n");
+        else
+          printf("Element 8 Found\n");
+        
+        temp = find_recursive(root,2);
+        if(temp==NULL)
+		  printf("Element 2 not found\n");
+        else
+          printf("Element 6 Found\n");
+        
+	
 		int path[height];
 		print_longest_path(root,path, height, 0);
 		
